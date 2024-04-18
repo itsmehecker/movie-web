d07254b0015d56ecdfdc4ba3355052ed9e18f62f
itsmehecker <90813931+itsmehecker@users.noreply.github.com>
2024-04-18 17:10:23 +0530

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,10 +1,10 @@
 window.__CONFIG__ = {
   // The URL for the CORS proxy, the URL must NOT end with a slash!
   // If not specified, the onboarding will not allow a "default setup". The user will have to use the extension or set up a proxy themselves
-  VITE_CORS_PROXY_URL: "",
+  VITE_CORS_PROXY_URL: "https://celadon-crepe-e6af54.netlify.app",
 
   // The READ API key to access TMDB
-  VITE_TMDB_READ_API_KEY: "",
+  VITE_TMDB_READ_API_KEY: "4b983dd2195d5f0313c5d8167c4f4ccc",
 
   // The DMCA email displayed in the footer, null to hide the DMCA link
   VITE_DMCA_EMAIL: null,
